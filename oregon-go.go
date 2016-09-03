677b0ed1738b151abc9ed984d4ddc0251f51f204
Matias Korhonen <matias@kiskolabs.com>
2016-09-04 00:46:15 +0300

    Initial TOML config file loading

--- oregon-go.go
+++ oregon-go.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
 
+	"github.com/BurntSushi/toml"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/iotdataplane"
 	"github.com/matiaskorhonen/oregon-go/oregonpi"
 )
+
+// ConfiguredSensor is used to configure which sensors are being listened for
+type ConfiguredSensor struct {
+	SensorType      int    `toml:"type"`
+	Channel         int    `toml:"channel"`
+	ThingEndpoint   string `toml:"thing_endpoint"`
+	SkipHumidity    bool   `toml:"skip_humidity"`
+	SkipTemperature bool   `toml:"skip_temperature"`
+}
+
+// Config is used to confifigure the GPIO pins and what sensors to listen for
+type Config struct {
+	RXPin   int                `toml:"rx_pin"`
+	TXPin   int                `toml:"tx_pin"`
+	Sensors []ConfiguredSensor `toml:"sensors"`
+}
 
 type thingState struct {
 	SensorName       string  `json:"sensorName"`
@@ -21,9 +40,38 @@ type thingState struct {
 	Humidity         float32 `json:"humidity"`
 }
 
+var config Config
+
+func init() {
+	var help bool
+	var configPath string
+
+	flag.StringVar(&configPath, "config", "", "path to the config file")
+	flag.BoolVar(&help, "help", false, "this help mesage")
+	flag.Parse()
+
+	if configPath == "" || help {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	tomlData, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	var conf Config
+	_, err = toml.Decode(string(tomlData), &conf)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	log.Println("Starting...")
-	monitor, err := oregonpi.NewSensorMonitor(1, 0)
+	monitor, err := oregonpi.NewSensorMonitor(config.RXPin, config.TXPin)
 
 	if err != nil {
 		log.Println(err)
@@ -55,7 +103,7 @@ func updateThingShadow(reading *oregonpi.SensorReading) {
 		return
 	}
 
-	svc := iotdataplane.New(sess, aws.NewConfig().WithEndpoint(os.GetEnv("AWS_IOT_ENDPOINT")))
+	svc := iotdataplane.New(sess, aws.NewConfig().WithEndpoint(os.Getenv("AWS_IOT_ENDPOINT")))
 
 	reportedState := thingState{
 		SensorName:       reading.Sensor.Name,
@@ -81,7 +129,7 @@ func updateThingShadow(reading *oregonpi.SensorReading) {
 		Payload:   payload,
 		ThingName: aws.String("OutsideWeatherSensor"),
 	}
-	_, err := svc.UpdateThingShadow(params)
+	_, err = svc.UpdateThingShadow(params)
 
 	if err != nil {
 		// Print the error, cast err to awserr.Error to get the Code and
